57262b6248d0cff230b3993493473b386cb33da5
ndimitrokalis <ndimitrokalis@nyc.gr>
2026-03-25 19:10:59 +0200

    Added Role Based Login/Register Logic

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,21 @@ login_manager = LoginManager()
 def startup():
     app = Flask(__name__)
     app.config.from_object("config.Config")
+ 
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+ 
+    from .models.user import User
+ 
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+ 
+    from .routes.auth import auth_bp
+    app.register_blueprint(auth_bp)
+ 
+    with app.app_context():
+        db.create_all()
+ 
     return app
